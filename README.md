1bcee036805deca0827f43c8db745c10bb87c282
danubiobwm <danubio.bwm@gmail.com>
2020-12-05 00:07:26 -0300

    finish Crud Flutter

---
+++ README.md
@@ -0,0 +1,6 @@
+# flutter_crud
+
+A new Flutter project.
+
+## Getting Started
+
---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_crud/models/users.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'João',
+    email: 'jao@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Maria',
+    email: 'maria@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Homem',
+    email: 'homen@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png',
+  ),
+};
